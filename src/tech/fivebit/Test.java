60130567984f9c6b7521e9558418c59c26a8d913
AFedorov <Aleksey.G.Fedorov@gmail.com>
2019-07-07 21:03:55 +0300

    First commit

--- src/tech/fivebit/Test.java
+++ src/tech/fivebit/Test.java
@@ -1,7 +1,16 @@
 package tech.fivebit;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class Test {
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
     public static void main (String[] args) {
+        Mat image = Imgcodecs.imread();
 
     }
 }
